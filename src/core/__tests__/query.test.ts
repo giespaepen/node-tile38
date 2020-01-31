3cc4c8b5a59e11ecb8464974ff6c2899afc09858
Gie Spaepen <giespaepen@outlook.com>
2020-01-31 15:12:04 +0100

    Added tests, added Travis

--- src/core/__tests__/query.test.ts
+++ src/core/__tests__/query.test.ts
@@ -9,12 +9,103 @@ describe("Tests for the Query class", () => {
         expect(query).not.toBeNull();
     });
 
-    it("Should create a query with cursor", () => {
+    it("Should create a query with CURSOR", () => {
         // Arrange
         const expected = "NEARBY test CURSOR 1 POINT 0 0";
         const query = new Tile38Query("NEARBY", "test")
             .withPoint(0, 0)
             .withCursor(1);
+
+        // Act
+        const actual = query.toString();
+
+        expect(actual).toBe(expected);
+    });
+
+    it("Should create a query with LIMIT", () => {
+        // Arrange
+        const expected = "NEARBY test LIMIT 1 POINT 0 0";
+        const query = new Tile38Query("NEARBY", "test")
+            .withPoint(0, 0)
+            .withLimit(1);
+
+        // Act
+        const actual = query.toString();
+
+        expect(actual).toBe(expected);
+    });
+
+    it("Should create a query with SPREAD", () => {
+        // Arrange
+        const expected = "NEARBY test SPARSE 1 POINT 0 0";
+        const query = new Tile38Query("NEARBY", "test")
+            .withPoint(0, 0)
+            .withSparse(1);
+
+        // Act
+        const actual = query.toString();
+
+        expect(actual).toBe(expected);
+    });
+
+    it("Should create a query with MATCH", () => {
+        // Arrange
+        const expected = "NEARBY test MATCH truck* POINT 0 0";
+        const query = new Tile38Query("NEARBY", "test")
+            .withPoint(0, 0)
+            .withMatch("truck*");
+
+        // Act
+        const actual = query.toString();
+
+        expect(actual).toBe(expected);
+    });
+
+    it("Should create a query with MATCH", () => {
+        // Arrange
+        const expected = "NEARBY test MATCH truck* POINT 0 0";
+        const query = new Tile38Query("NEARBY", "test")
+            .withPoint(0, 0)
+            .withMatch("truck*");
+
+        // Act
+        const actual = query.toString();
+
+        expect(actual).toBe(expected);
+    });
+
+    it("Should create a query with DISTANCE", () => {
+        // Arrange
+        const expected = "NEARBY test DISTANCE POINT 0 0";
+        const query = new Tile38Query("NEARBY", "test")
+            .withPoint(0, 0)
+            .withDistance();
+
+        // Act
+        const actual = query.toString();
+
+        expect(actual).toBe(expected);
+    });
+
+    it("Should create a query with WHERE", () => {
+        // Arrange
+        const expected = "NEARBY test WHERE speed 30 60 POINT 0 0";
+        const query = new Tile38Query("NEARBY", "test")
+            .withPoint(0, 0)
+            .withWhere("speed", 30, 60);
+
+        // Act
+        const actual = query.toString();
+
+        expect(actual).toBe(expected);
+    });
+
+    it("Should create a query with WHEREIN", () => {
+        // Arrange
+        const expected = "NEARBY test WHEREIN speed 2 30 60 POINT 0 0";
+        const query = new Tile38Query("NEARBY", "test")
+            .withPoint(0, 0)
+            .withWhereIn("speed", 30, 60);
 
         // Act
         const actual = query.toString();
